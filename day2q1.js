cc0cb050509081b27e08f0f3b9f6c4aee6a4c42e
namita2601 <69844115+namita2601@users.noreply.github.com>
2020-09-01 15:15:10 +0530

    Add files via upload

---
+++ day2q1.js
@@ -0,0 +1,6 @@
+let name = "ankita";
+let character = "a";
+console.log("The string is "+name);
+console.log("the character to be searched is:"+character);
+let index = name.indexOf(character);
+console.log("The character "+character+" was found at "+index+"th position");
